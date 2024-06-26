f111b26ff45e7854f00f76291b7b70445f14606f
dzmitry <dmitrynish@mail.ru>
2024-06-26 19:02:11 +0100

    feature selection pipe: minor updates

--- src/bcase2_sales_forecasting/pipelines/p04_feature_selection/utils.py
+++ src/bcase2_sales_forecasting/pipelines/p04_feature_selection/utils.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import re
 from typing import Any, Dict
+from scipy.stats import shapiro
 
 
 def debug_on_success_(data: pd.DataFrame, dummy_value: int, pipeline_name: str = "", f_verbose: bool = False) -> None:
@@ -14,3 +15,11 @@ def debug_on_success_(data: pd.DataFrame, dummy_value: int, pipeline_name: str =
     print(f"pipeline {pipeline_name} succeed !; f_verbose={f_verbose};", dummy_value)
 
     return
+
+
+def check_normality(data):
+    """Function to check normality using Shapiro-Wilk test
+    """
+    _, p_value = shapiro(data)
+    
+    return p_value > 0.05  # Null hypothesis: data is normally distributed if p_value > 0.05
