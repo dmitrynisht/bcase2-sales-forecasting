ea60482d38af49e4ff5c76c0b3d44f4946bad7eb
Jannik Himmelsbach <Jannik.Himmelsbach@gmx.de>
2024-06-28 18:12:42 +0100

    added test data ingestion

--- src/bcase2_sales_forecasting/pipelines/p02_ingested/pipeline.py
+++ src/bcase2_sales_forecasting/pipelines/p02_ingested/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.18.8
 
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import  ingest_sales, ingest_markets, ingest_gdp
+from .nodes import  ingest_sales, ingest_markets, ingest_gdp , ingest_test_data
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -28,6 +28,12 @@ def create_pipeline(**kwargs) -> Pipeline:
                 inputs=["german_gdp_raw_data", "parameters", "02_dummy"],
                 outputs=["ingested_german_gdp", "03_dummy"],
                 name="ingest_german_gdp_node",
+            ),
+            node(
+                func=ingest_test_data,
+                inputs=["test_raw_data", "parameters"],
+                outputs="ingested_test_data",
+                name="ingest_test_node",
             ),
         ]
     )
