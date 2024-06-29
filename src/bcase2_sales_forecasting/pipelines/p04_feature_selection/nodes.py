5e64b9cf1abd14eb55a635f76ae5bd5159e06ee3
carlos-rodrigues75 <carlos.rodrigues.dev@gmail.com>
2024-06-29 12:20:19 +0100

    Add feat selection node to calculate lag features

--- src/bcase2_sales_forecasting/pipelines/p04_feature_selection/nodes.py
+++ src/bcase2_sales_forecasting/pipelines/p04_feature_selection/nodes.py
@@ -63,3 +63,60 @@ def market_features_selection(
         debug_on_success_(market_copy, dummy_value, pipeline_name, f_verbose)
 
     return market_copy, dummy_value
+
+def compute_sales_lag_features(
+        sales_data: pd.DataFrame,
+        market_data: pd.DataFrame,
+        parameters: Dict[str, Any]) -> pd.DataFrame:
+    # Creating a dataframe for each product sales
+    product_sales_map = {}
+    for product_name in sales_data["gck"].unique():
+        # Modify product name to ensure it's a valid variable name
+        valid_product_name = re.sub(r'\W+', '_#', product_name)
+
+        # Dynamically create variable name based on product name
+        product_sales_map[f"sales{valid_product_name}"] = sales_data[sales_data["gck"] == product_name]
+
+    # Set 'Month Year' as index
+    market_data['month_year'] = pd.to_datetime(market_data['month_year'])
+    market_data.set_index('month_year', inplace=True)
+
+    # Define lag periods
+    lag_periods = range(1, 13)
+
+    # Create a dictionary to store lagged datasets
+    mkt_lagged_datasets = {}
+
+    # Generate lagged datasets
+    for lag in lag_periods:
+        start_date = pd.to_datetime('2018-09-01') - pd.DateOffset(months=lag)
+        end_date = pd.to_datetime('2022-03-01') - pd.DateOffset(months=lag)
+        mkt_lagged_datasets[f'market_lag{lag}'] = market_data[start_date:end_date].reset_index(drop=True)
+    
+    sales_lag_per_product = {}
+
+    # Loop through each product group
+    for product_code in sales_data["gck"].unique():
+        sales_lag = find_lag(product_code, market_data, product_sales_map, mkt_lagged_datasets)
+        sales_lag.set_index("full_date", inplace=True)
+        sales_lag.drop("gck", axis=1, inplace=True)
+
+        sales_lag_per_product[product_code] = sales_lag
+
+    lag_datasets = sales_lag_per_product.values()
+
+    # Apply the function to each lag dataset
+    for lag_data in lag_datasets:
+        add_sales_lags(lag_data)
+        lag_data.dropna(inplace=True)
+
+    # Only return sales of product specified in parameters
+    # fallback to #1 if not specified
+    product_code = parameters.get(product_code) or "#1"
+    sales_p1 = sales_lag_per_product["#1"]
+
+    #TODO: Create specific nodes in pipeline to get corr and top feats
+    sales_p1 = sales_p1[list(set(['sales_eur'] + get_highly_correlated_features(sales_p1) + get_top_10_features(sales_p1)))]
+    sales_p1.dropna(inplace=True)
+
+    return [sales_p1]
--- src/bcase2_sales_forecasting/pipelines/p04_feature_selection/pipeline.py
+++ src/bcase2_sales_forecasting/pipelines/p04_feature_selection/pipeline.py
@@ -1,6 +1,6 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import market_features_selection
+from .nodes import market_features_selection, compute_sales_lag_features
 
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -12,5 +12,11 @@ def create_pipeline(**kwargs) -> Pipeline:
                 outputs=["markets_feature_selection", "07_dummy"],
                 name="markets_collinearity_elimination",
             ),
+            node(
+                func=compute_sales_lag_features,
+                inputs=['preprocessed_sales', 'markets_feature_selection', 'parameters'],
+                outputs=["features_with_sales_lag"],
+                name="compute_sales_lag_features",
+            )
         ]
     )
