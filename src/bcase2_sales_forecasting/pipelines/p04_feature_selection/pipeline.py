4e1f36f731ab54806b40a655f9585eb5ae461e3a
Jannik Himmelsbach <Jannik.Himmelsbach@gmx.de>
2024-06-30 16:53:49 +0100

    minor changes

---
+++ src/bcase2_sales_forecasting/pipelines/p04_feature_selection/pipeline.py
@@ -0,0 +1,22 @@
+from kedro.pipeline import Pipeline, node, pipeline
+
+from .nodes import market_features_selection, compute_sales_lag_features
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=market_features_selection,
+                inputs=["processed_markets", "parameters"],
+                outputs="markets_best_features",
+                name="markets_collinearity_elimination",
+            ),
+            node(
+                func=compute_sales_lag_features,
+                inputs=['preprocessed_sales', 'markets_best_features', 'parameters'],
+                outputs=["features_with_sales_lag", "feature_importance_plot"],
+                name="compute_sales_lag_features",
+            )
+        ]
+    )
